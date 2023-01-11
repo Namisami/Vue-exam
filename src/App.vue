2d2ef83d2de5b4b67c50c3c727018ce81527d9d5
nami <warriorparovoz@gmail.com>
2023-01-11 12:31:35 +0300

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="app" class="">
-    <header class="bg-dark d-flex justify-content-between px-5 py-2 d-print-none">
+    <header class="bg-dark d-flex flex-column flex-sm-row justify-content-between px-5 py-2 d-print-none">
       <nav class="nav">
         <v-tabs dark>
           <v-tab
@@ -17,19 +17,19 @@
           </v-tab>
         </v-tabs>
       </nav>
-      <a href="/albums.json" target="_blank" class="btn btn-secondary text-white py-3 mx-2">
+      <a href="/albums.json" target="_blank" class="my-1 btn btn-secondary text-white py-3">
         albums.json
       </a>
-      <a href="/images.json" target="_blank" class="btn btn-secondary text-white py-3">
+      <a href="/images.json" target="_blank" class="my-1 btn btn-secondary text-white py-3">
         images.json
       </a>
-      <div class="d-flex">
-        <button v-if="user.name" class="btn btn-primary text-light">{{ user.name }}</button>
+      <div class="d-flex justify-content-center">
+        <button v-if="user.name" class="my-1 btn btn-primary text-light">{{ user.name }}</button>
         <router-link class="d-flex" v-else to="/login">
-          <button type="button" class="btn btn-primary text-light">Login</button>
+          <button type="button" class="my-1 btn btn-primary text-light">Login</button>
         </router-link>
-        <button v-if="user.name" class="btn btn-warning ml-2" @click="logout">Log out</button>
-        <button v-else class="btn btn-warning ml-2">Register</button>
+        <button v-if="user.name" class="my-1 btn btn-warning ml-2" @click="logout">Log out</button>
+        <button v-else class="my-1 btn btn-warning ml-2">Register</button>
       </div>
     </header>
     <v-main>
--- src/views/ImageView.vue
+++ src/views/ImageView.vue
@@ -4,10 +4,9 @@
       Image Element
     </h1>
     <img 
-      class="rounded mx-auto d-block"
+      class="rounded mx-auto d-block w-75"
       v-if="image.cover !== null"
       :src="image.cover && require(`../../public/images/${image.cover}`)"
-      width="500"  
       alt="Big version of picture"
     />
     <div class="alert alert-info my-2">
